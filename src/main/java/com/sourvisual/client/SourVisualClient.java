b5e97eb2913e1d5da194a58ec9445b41341a3b4b
sourvisual2 <rubik20231@gmail.com>
2026-08-07 01:32:27 +0300

    Update SourVisualClient.java

--- src/main/java/com/sourvisual/client/SourVisualClient.java
+++ src/main/java/com/sourvisual/client/SourVisualClient.java
@@ -4,6 +4,7 @@ import com.sourvisual.client.gui.VisualMenuScreen;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
 import org.lwjgl.glfw.GLFW;
@@ -23,10 +24,15 @@ public class SourVisualClient implements ClientModInitializer {
 
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             while (openMenuKey.wasPressed()) {
-                if (client.currentScreen == null) {
+                // если меню открыто — закрыть
+                if (client.currentScreen instanceof VisualMenuScreen) {
+                    client.setScreen(null);
+                }
+                // если меню закрыто — открыть
+                else if (client.currentScreen == null) {
                     client.setScreen(new VisualMenuScreen());
                 }
             }
         });
     }
-}
+    }

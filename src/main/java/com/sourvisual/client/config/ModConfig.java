d5c89711af0a565aa635e678f8a789eb817f3b46
sourvisual2 <rubik20231@gmail.com>
2026-08-07 14:12:44 +0300

    Update ModConfig.java

--- src/main/java/com/sourvisual/client/config/ModConfig.java
+++ src/main/java/com/sourvisual/client/config/ModConfig.java
@@ -10,10 +10,20 @@ import java.nio.file.*;
 public class ModConfig {
 
     public static boolean hitColorEnabled = false;
+    // цвет в формате RGB int (по умолчанию фиолетовый)
+    public static int hitColorR = 123;
+    public static int hitColorG = 0;
+    public static int hitColorB = 255;
 
     private static final Gson GSON = new GsonBuilder().setPrettyPrinting().create();
     private static final Path CONFIG_PATH =
             FabricLoader.getInstance().getConfigDir().resolve("sourvisual.json");
+
+    // для сериализации
+    public boolean hitColorEnabled_s = false;
+    public int hitColorR_s = 123;
+    public int hitColorG_s = 0;
+    public int hitColorB_s = 255;
 
     public static void load() {
         try {
@@ -22,23 +32,30 @@ public class ModConfig {
                 ModConfig data = GSON.fromJson(reader, ModConfig.class);
                 reader.close();
                 if (data != null) {
-                    hitColorEnabled = data.hitColorEnabled;
+                    hitColorEnabled = data.hitColorEnabled_s;
+                    hitColorR       = data.hitColorR_s;
+                    hitColorG       = data.hitColorG_s;
+                    hitColorB       = data.hitColorB_s;
                 }
             }
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        } catch (Exception e) { e.printStackTrace(); }
     }
 
     public static void save() {
         try {
             ModConfig data = new ModConfig();
-            data.hitColorEnabled = ModConfig.hitColorEnabled;
+            data.hitColorEnabled_s = hitColorEnabled;
+            data.hitColorR_s       = hitColorR;
+            data.hitColorG_s       = hitColorG;
+            data.hitColorB_s       = hitColorB;
             Writer writer = Files.newBufferedWriter(CONFIG_PATH);
             GSON.toJson(data, writer);
             writer.close();
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
+        } catch (Exception e) { e.printStackTrace(); }
+    }
+
+    // Возвращает ARGB int для рендерера
+    public static int getHitColor() {
+        return (255 << 24) | (hitColorR << 16) | (hitColorG << 8) | hitColorB;
     }
 }

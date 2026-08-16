8840b1e244e11b8e0e86163b22c656b5adbbbb8c
sourvisual2 <rubik20231@gmail.com>
2026-08-16 21:28:00 +0300

    Create SourVisual.java

---
+++ src/main/java/com/sourvisual/SourVisual.java
@@ -0,0 +1,12 @@
+package com.sourvisual;
+
+import net.fabricmc.api.ModInitializer;
+
+public class SourVisual implements ModInitializer {
+    public static final String MOD_ID = "sourvisual";
+
+    @Override
+    public void onInitialize() {
+        // Общая/серверная логика мода (если понадобится в будущем)
+    }
+}

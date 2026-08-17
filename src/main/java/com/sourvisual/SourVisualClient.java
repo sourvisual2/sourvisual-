50f83e46181198946291088c8ad3eb7ebcdf517a
sourvisual2 <rubik20231@gmail.com>
2026-08-17 22:36:14 +0300

    Update SourVisualClient.java

--- src/main/java/com/sourvisual/SourVisualClient.java
+++ src/main/java/com/sourvisual/SourVisualClient.java
@@ -2,10 +2,12 @@ package com.sourvisual;
 
 import com.sourvisual.gui.VisualScreen;
 import com.sourvisual.hud.Watermark;
+import com.sourvisual.render.HitboxRenderer;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.fabricmc.fabric.api.client.rendering.v1.HudRenderCallback;
+import net.fabricmc.fabric.api.client.rendering.v1.WorldRenderEvents;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
@@ -36,5 +38,7 @@ public class SourVisualClient implements ClientModInitializer {
 
         HudRenderCallback.EVENT.register((drawContext, tickDelta) ->
                 Watermark.render(drawContext, MinecraftClient.getInstance()));
+
+        WorldRenderEvents.AFTER_ENTITIES.register(HitboxRenderer::render);
     }
 }
